bb35ab0f5aeb5023f7a109a9fee7387318120c65
Andriy Kushnir (Orhideous) <me@orhideous.name>
2023-11-28 23:50:24 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+#[macro_use]
+extern crate rocket;
+
+use reqwest::Client;
+use rocket::State;
+
+#[derive(Debug, PartialEq, FromForm)]
+struct CountryRequest<'a> {
+    country: &'a str,
+    name: &'a str,
+    timeout: &'a str,
+}
+fn process_cidr_block(blocks: &str, country_request: CountryRequest) -> String {
+    let CountryRequest {
+        country,
+        name,
+        timeout,
+    } = country_request;
+    blocks
+        .trim()
+        .split('\n')
+        .map(|s| {
+            format!(
+                ":do {{ add address={} list={} timeout={} comment={} }} on-error={{}}",
+                s, name, timeout, country
+            )
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+#[get("/?<list..>")]
+async fn list(list: CountryRequest<'_>, client: &State<Client>) -> Option<String> {
+    let url = format!(
+        "https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/{}.cidr",
+        list.country.to_lowercase()
+    );
+
+    match client.get(&url).send().await {
+        Ok(body) => {
+            debug!("Loaded {} country", &list.country);
+            body.text()
+                .await
+                .ok()
+                .as_deref()
+                .map(|block| process_cidr_block(block, list))
+        }
+        Err(error) => {
+            error!("Failed to obtain {} country: {:?}", &list.country, error);
+            None
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    let client = Client::new();
+
+    rocket::build().manage(client).mount("/", routes![list])
+}
