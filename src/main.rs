85b4f73654574fc560d9688e060fe2fd81bddc00
Andriy Kushnir (Orhideous) <me@orhideous.name>
2023-12-18 13:51:51 +0200

    Support ipv6 as well

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,50 @@ extern crate rocket;
 use reqwest::Client;
 use rocket::State;
 
+#[derive(Debug, PartialEq, FromFormField)]
+enum IPVersion {
+    V4,
+    V6,
+}
+
 #[derive(Debug, PartialEq, FromForm)]
 struct CountryRequest<'a> {
     country: &'a str,
     name: &'a str,
     timeout: &'a str,
+    version: IPVersion,
 }
 fn process_cidr_block(blocks: &str, country_request: CountryRequest) -> String {
     let CountryRequest {
         country,
         name,
         timeout,
+        version,
     } = country_request;
+    let header: String = match version {
+        IPVersion::V4 => "/ip firewall address-list".into(),
+        IPVersion::V6 => "/ipv6 firewall address-list".into(),
+    };
+    let mut commands = vec![header];
     let add_commands = blocks.trim().split('\n').map(|s| {
         format!(
             ":do {{ add address={} list={} timeout={} comment={} }} on-error={{}}",
             s, name, timeout, country
         )
     });
-    let mut commands = vec![String::from("/ip firewall address-list")];
     commands.extend(add_commands);
     commands.join("\n")
 }
 
 #[get("/api/v0/list?<list..>")]
 async fn list(list: CountryRequest<'_>, client: &State<Client>) -> Option<String> {
+    let ver = match list.version {
+        IPVersion::V4 => "ipv4",
+        IPVersion::V6 => "ipv6",
+    };
     let url = format!(
-        "https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/{}.cidr",
+        "https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/{}/{}.cidr",
+        ver,
         list.country.to_lowercase()
     );
 
